c5328c4750aa3c70a1519757ee8cb2b1acfee69b
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-08-09 13:04:55 -0500

    Added stub docstring for DiscontinuousHMC

--- pymc3/step_methods/hmc/dhmc.py
+++ pymc3/step_methods/hmc/dhmc.py
@@ -4,5 +4,14 @@ __all__ = ['DiscontinuousHMC']
 
 
 class DiscontinuousHMC(BaseHMC):
+    R"""A sampler for discrete parameters and likelihoods based on Hamiltonian mechanics.
+
+    A detailed description can be found at [1], "Discontinuous Hamiltonian Monte Carlo for 
+    sampling discrete parameters".
     
+    References
+    ----------
+    .. [1] Nishimura, A., Dunson, D., and Lu, J. 2017. Discontinuous Hamiltonian Monte Carlo 
+    for sampling discrete parameters. arXiv:1705.08510.
+    """
     pass
