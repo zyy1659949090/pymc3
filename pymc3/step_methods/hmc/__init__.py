6be8515c858ed7517cc4fc1eee9b38fffb6ac36e
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-08-09 13:01:13 -0500

    Added skeleton class for DiscontinuousHMC

--- pymc3/step_methods/hmc/__init__.py
+++ pymc3/step_methods/hmc/__init__.py
@@ -1,2 +1,3 @@
 from .hmc import HamiltonianMC
 from .nuts import NUTS
+from .dhmc import DiscontinuousHMC
---
+++ pymc3/step_methods/hmc/dhmc.py
@@ -0,0 +1,8 @@
+from .base_hmc import BaseHMC
+
+__all__ = ['DiscontinuousHMC']
+
+
+class DiscontinuousHMC(BaseHMC):
+    
+    pass
